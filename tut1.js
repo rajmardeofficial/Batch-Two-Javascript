96eb98aacf76fd3d1a6c45501d720b2ba24f331a
Raj Marde <rajmarde615@gmail.com>
2021-12-15 20:04:21 +0530

    JavaScript Tutorial

---
+++ tut1.js
@@ -0,0 +1,22 @@
+//() these brackets are called paranthesis
+//{} these brackets are called curly brackets
+
+// console.log('Coding')
+//to find errors or to debug the errors
+//to print the values in console of browser
+
+
+//VARIBLES//
+
+//In general mathematical operation in algebra
+
+// x = 3
+// y = 7
+
+// x+y = 10
+
+var x = 'Pavan'; //this is the syntax to declare a varible
+console.log(x);
+var y = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique cum error voluptatem recusandae beatae totam, mollitia placeat? Accusantium dicta fugit minima ipsa eaque et natus maxime cupiditate! Modi, molestias aspernatur!"
+var x = 'Raj';
+console.log(x);
