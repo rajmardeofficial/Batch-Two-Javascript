2253559083b0aa359dcbfd855a3ece2ee395dac9
Raj Marde <rajmarde615@gmail.com>
2021-12-16 20:23:09 +0530

    Tut 2

--- tut1.js
+++ tut1.js
@@ -15,8 +15,38 @@
 
 // x+y = 10
 
-var x = 'Pavan'; //this is the syntax to declare a varible
-console.log(x);
-var y = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique cum error voluptatem recusandae beatae totam, mollitia placeat? Accusantium dicta fugit minima ipsa eaque et natus maxime cupiditate! Modi, molestias aspernatur!"
-var x = 'Raj';
-console.log(x);
+// var x = 'Pavan'; //this is the syntax to declare a varible
+// console.log(x);
+// var y = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique cum error voluptatem recusandae beatae totam, mollitia placeat? Accusantium dicta fugit minima ipsa eaque et natus maxime cupiditate! Modi, molestias aspernatur!"
+// var x = 'Raj';
+// console.log(x);
+
+//Task
+//to do the addition of two variables
+
+// var a = 2; //container one
+// var b = 3; //container two
+
+// var c = a+b; //container three which containes a and b container
+
+// var c = a*b;
+
+// console.log(c);
+
+//You can also declare variable like this
+//You can also reassign the variable in var keyword
+
+// var a;
+
+// a = 2;
+
+// console.log(a);
+
+//using quotes or inverted commas inside each other
+
+// var a = "pavan's bat";
+
+
+
+
+
---
+++ tut2.js
@@ -0,0 +1,80 @@
+//let//
+// difference between var and let
+
+//RULE 1//
+
+// WE CANNOT REDCLARE THE VARIABLE IN let
+
+// let x = 2;
+// console.log(x);
+
+// let x = 4; // wrong syntax
+
+// WE CAN REDCLARE THE VARIABLE IN var
+
+// var a = 'Prathamesh';
+
+// var a = 2;
+
+// console.log(a);
+
+//RULE 2//
+
+//VARIABLE DECLARED WITH let KEYWORD MUST BE DECLARED BEFORE USE
+
+// let a = 4; //we have declared the value here
+
+// console.log(a); //we have used a here
+
+//wrong method
+
+// console.log(b); //we have used a here
+
+// let b = 5;//we have declared the value here
+
+
+//HERE IS THE DIFFERENCE FOR ABOVE EXAMPLE WITH var KEYWORD
+
+//Instructions
+//In var we can declare the variable before use 
+//but you must always declare the variable before use in any case as a standard procedure and to avoid mistakes.
+
+//compare with var 
+
+// console.log(x); 
+
+// var x = 2;
+
+//RULE 3//
+
+//BLOCK SCOPE//
+
+//block scope means declaring the varibles inside the curly brackets {}
+
+//DECLARING THE VARIABLE INSIDE BLOCK
+
+//{
+    //let x = 10; // variable is declared
+    //console.log(x); // we have  used a here
+
+    //we can use x here where x = 10
+//}
+
+// console.log(x); // we cannot use x here because it is defined using let inside {} block
+
+// BLOCK SCOPE IN var
+
+//We must use let keyword if we are facing the problem given below
+
+
+// var y = 2;
+// // value of y is 2
+// {
+//     var y = 10;  
+//     console.log(y);
+
+//     //here the value of y is 10
+// }
+
+// console.log(y);
+// //here the value of y is 10
