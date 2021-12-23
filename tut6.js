a6fd11ad80fb5946cac1ad7b459b75516c6fea8a
Raj Marde <rajmarde615@gmail.com>
2021-12-23 19:54:32 +0530

    Tut 6

---
+++ tut6.js
@@ -0,0 +1,50 @@
+/////////String, cancatenation and its methods/////////////
+
+//String is a data type which is always written in double or single quotes
+//eg
+
+// let x = "Steve smith";
+
+// console.log(typeof x);
+
+///////Concatenation////////
+
+// let a = "His name is ";
+
+// let b = "Steve";
+
+// let c = "His name is " + "Steve";
+
+// let x = "what is the first number " + 6;
+
+// console.log(x);
+
+///////TASK///////
+
+//create a prompt and identify the data type of given input
+
+//1.toUpperCase()
+
+// let x = "cricket";
+
+// let y = x.toUpperCase();
+
+// console.log(y);
+
+//2.toLowerCase()
+
+// let x = 'HELLO';
+
+// let y = x.toLowerCase();
+
+// console.log(y);
+
+//split and slice to cover later
+
+//3.replace()
+
+// let b = "VsCode";
+
+// let a = b.replace("VsCode", "NOTEPAD");
+
+// console.log(a);
