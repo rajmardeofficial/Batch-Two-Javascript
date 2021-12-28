f1fbb8042dda3b16bcbe8c33ca6ef1a27a08679e
Raj Marde <rajmarde615@gmail.com>
2021-12-28 19:57:38 +0530

    Tut 7

---
+++ tut7.js
@@ -0,0 +1,63 @@
+//////////Tempelate Literals//////////
+
+//Syntax//
+// let a = `Hello World` // ``(back tics) are used as for template literas
+
+// let x = "Pavan's bat" //limitations for using quotes
+
+// let z = `He's called as "Johny"` //we can use both double and single quotes inside one another
+
+// let b = `he is a good 
+// boy`
+
+
+//Using it as a Tempelate or Interpolation
+//Tempelate literals allows us to use the variables inside the string
+
+// let x = 123;
+
+// alert('the number is '+x)
+
+// alert(`the number is ${x}`)
+
+// alert('the number id ${x}')
+
+// let name = 'Raj';
+
+// alert(`hello ${name} u r welcome`)
+
+
+////////////Arrays/////////////////
+//An array is a special variable, which can hold more than one value
+
+//syntax
+// let array = ["Raj", "Suyash"]; //we delcared array as a value to variable here
+
+// let b = [
+//     'raj',
+//     'steve',
+//     123,
+// ]
+
+// let b = ['raj', 'steve', 123]
+
+// let a = 123;
+
+// let value = array[0] //the number inside the [] is known as index
+
+// alert(value)
+
+
+// let car = ["Brezza", "BMW", "Alto", "Mercedes"]
+
+// car[1] = "Swift"
+
+// alert(car.length)
+
+
+// var a = Number(prompt("What is the first number"));
+// var b = Number(prompt("What is the seconf number to be added"));
+
+// alert(a+b)
+
+// console.log(typeof(a,b));
