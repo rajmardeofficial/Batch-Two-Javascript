ab0e61566a590120c231bea8d4e055ac1b3e53f8
Raj Marde <rajmarde615@gmail.com>
2021-12-31 20:03:40 +0530

    Tutorial 10

---
+++ tut10.js
@@ -0,0 +1,10 @@
+///////////Functions ES6//////////
+//Functions is a block of code designed to performed a perticular tasks
+//to excecute the function we have to call the function
+
+//Syntax
+function functionName() {
+  //code to be excecuted
+}
+
+functionName();
--- tut6.js
+++ tut6.js
@@ -1,4 +1,4 @@
-/////////String, cancatenation and its methods/////////////
+/////////String, concatenation and its methods/////////////
 
 //String is a data type which is always written in double or single quotes
 //eg
@@ -14,6 +14,7 @@
 // let b = "Steve";
 
 // let c = "His name is " + "Steve";
+// console.log(c);
 
 // let x = "what is the first number " + 6;
 
@@ -48,3 +49,21 @@
 // let a = b.replace("VsCode", "NOTEPAD");
 
 // console.log(a);
+
+
+//4.slice()
+//It is used to extract the part of the string
+//Syntax
+//a.slice(7,13)
+
+// let a = "Apple, Banana, Kiwi";   
+// // let b = ["Apple", "Banana", "Kiwi"]
+
+// let z = a.slice(7, 13)
+
+// console.log(z);
+
+
+// let y = a.slice(-12, -6)
+
+// console.log(y);
