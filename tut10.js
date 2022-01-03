aee773ba531c08be890965d70245017c44b42b7a
Raj Marde <rajmarde615@gmail.com>
2022-01-03 20:09:08 +0530

    Functions covered

--- tut10.js
+++ tut10.js
@@ -2,9 +2,96 @@
 //Functions is a block of code designed to performed a perticular tasks
 //to excecute the function we have to call the function
 
-//Syntax
-function functionName() {
-  //code to be excecuted
-}
+// //Syntax
+// function functionName() {
+//   //code to be excecuted
+// }
 
-functionName();
+// //calling functions
+
+// functionName();
+
+
+
+// function alert1(){
+//   //variable declared inside the function cannot be used outside the function
+//   let a = 10
+//   alert('Hello World')
+// }
+
+// console.log(a);
+
+// alert1()
+
+
+//using onclick();
+
+// function functionTwo(){
+//   alert('This function is invoked after onclick')
+//   console.log('hello');
+// }
+
+
+// let a = 123 //global variable
+
+// console.log(a);
+
+//parameters are declare to perform more advance code of functions
+
+// let a;
+
+// a = 10
+
+// console.log(a);
+
+// function functionThree(p1){
+//   //here we say parameters
+//   console.log(p1);
+// }
+
+// functionThree("Raj"); //here we say arguments
+
+
+// function add(){
+//   let p1 = Number(prompt("What is the first number"));
+//   let p2 = Number(prompt("What is the Second number"));
+//   alert(p1 + p2)
+// }
+
+// add()
+
+// function add (p1, p2){
+//   alert(p1 + p2)
+// }
+
+// add(1 , 2)
+
+
+///TASK///
+//create four functions of add, subtract, multiply, division
+//get the result by providing the values through parameters
+//try making the function for BMI
+
+
+// let user = {
+//   name: "Raj",
+//   roll: 02,
+//   address: "Niphad"
+// }
+
+// console.log(user);
+
+
+// function Person(name, roll, address){
+//   this.name = name
+//   this.roll = roll
+//   this.address = address;
+// }
+
+// const p1 = new Person("Raj", 02, "Niphad")
+// const p2 = new Person("Aryan", 03, "Niphad")
+// const p3 = new Person("Vaishnavi", 04, "Niphad");
+
+// console.log(p1);
+
+
