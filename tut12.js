29d3a807fc21a4b61c18ad0e68ccb19e0dacc9b6
Raj Marde <rajmarde615@gmail.com>
2022-01-15 20:05:08 +0530

    control flow statement

---
+++ tut12.js
@@ -0,0 +1,140 @@
+// let a = 10
+
+
+// //increment
+// console.log(++a);
+
+
+// //decrement
+// console.log(--a);
+
+// a = a+5
+
+// console.log(a);
+
+// a += 5
+
+// console.log(a);
+
+// a -= 5;
+
+// console.log(a);
+
+
+
+// a *= 5;
+
+// console.log(a);
+
+
+// a /= 5;
+
+// console.log(a);
+
+
+// a %= 3;
+
+// console.log(a);
+
+
+// a **= 3;
+
+// console.log(a);
+
+
+////Comparision Operators////
+
+// let a = 10
+
+// console.log(a=='10');
+
+
+// console.log(a === 10);
+
+//not equal
+
+// console.log(a!=10);
+
+// console.log(a<=10);
+
+// 7 > 10 false
+// 7 < 10 true
+// 7 <= 10 false
+// 10 >= 10 true
+
+///Logical Operators///
+
+//1. && Logical and
+//2. || Logical or
+//3. ! Logical not
+
+// let a = 10
+// let b = 12
+
+//1. &&
+// console.log(a === 13 && b === 100);
+
+//truth table(and logical operator)
+
+//true     true    =     true
+//true     false    =     false
+//false     true    =     false
+//false     false    =     false
+
+
+//2. ||
+
+// console.log(a === 10 || b === 100);
+
+//truth table(or logical operator)
+
+//true     true    =     true
+//true     false    =     true
+//false     true    =     true
+//false     false    =     false
+
+//if else statement OR control flow statement OR logical statements
+
+//syntax
+
+// if(true condition){
+//     //code to be executed
+// } else {
+// //code to be executed when given condition is false
+// }
+
+//EXAMPLE
+
+// let a = 10
+
+// if(a === 10){
+//     console.log(`the value of a is ${a}`);
+// } else {
+//     console.log(`the value of a is not 10`)
+// }
+
+// if(usersPassword === 'rajmarde'){
+//     //redirect user to main page
+// } else {
+//     //send error to login message
+// }
+
+///Real life example OR Real world example
+//nest
+
+
+// if(usersPassword === 'rajmarde'){
+//     if(phone === 1234567890){
+//         //land to main page
+//     } else {
+//         //check ur second level of security
+//     }
+// } else {
+//     //else code for first condition
+// }
+
+
+
+// if (a === 12){
+//     console.log('yeee');
+// } 
